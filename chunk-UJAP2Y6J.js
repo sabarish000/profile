6bb12cbfa7bffff51815ab2e4a1fabaa5f4819ea
Sabarish Karrothu <sabarish00@yahoo.in>
2024-12-10 17:24:57 +0100

    app build

---
+++ chunk-UJAP2Y6J.js
@@ -0,0 +1,1 @@
+import{Ja as n,Ka as s,Pa as p,wa as o}from"./chunk-FIO2Y34K.js";var c=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-projects"]],decls:2,vars:0,template:function(e,a){e&1&&(n(0,"p"),p(1,"projects works!"),s())},encapsulation:2})};export{c as ProjectsComponent};
---
+++ chunk-XSYL325O.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-QMCCUXHM.js";import"./chunk-C635INN3.js";import"./chunk-FIO2Y34K.js";export{a as ContactComponent};
